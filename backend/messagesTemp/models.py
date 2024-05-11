909312384acad173fc84804ae4ba9e16206199e1
TomatoFT <20520926@gm.uit.edu.vn>
2024-05-12 00:37:39 +0700

    Undo the changed in the admin

---
+++ backend/messagesTemp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Notification(models.Model):
+
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     app_label = 'messagesTemp'
+
+    def __str__(self):
+        return self.content
---
+++ backend/messagesTemp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Notification
+from .serializers import MessageSerializer
+
+
+@api_view(["GET"])
+def get_data(request):
+    messages = Notification.objects.all()
+    serializers = MessageSerializer(messages, many=True)
+    return Response(serializers.data)
+
+
+@api_view(["POST"])
+def add_data(request):
+    serializers = MessageSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+    return Response(serializers.data)
+
+
+@api_view(["PUT"])
+def update_data(request, content):
+    messages = Notification.objects.get(content=content)
+    data = MessageSerializer(instance=messages, data=request.data)
+    print("DATA:", data)
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(["DELETE", "GET"])
+def delete_data(request, content):
+    item = get_object_or_404(Notification, content=content)
+    item.delete()
+    return redirect(reverse("get_data"))
