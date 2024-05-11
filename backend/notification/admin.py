d7512894298d53e93bf8549dcd134e5f0f770f78
TomatoFT <20520926@gm.uit.edu.vn>
2024-05-12 01:15:17 +0700

    Remove the temp message and clean budget admin

--- backend/notification/admin.py
+++ backend/notification/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<<< HEAD:backend/notification/admin.py
 from notification.models import Notification
-========
-
-from .models import Notification
->>>>>>>> main:backend/messagesTemp/admin.py
 
 # Register your models here.
 admin.site.register(Notification)
